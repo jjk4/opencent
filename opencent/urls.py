f176b8dec71c4fca2283a4fd03a0814b2328be14
jjk4 <shaun7090@gmail.com>
2026-03-30 10:33:51 +0200

    fix again

--- opencent/urls.py
+++ opencent/urls.py
@@ -18,12 +18,17 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.urls import re_path
+from django.views.static import serve
 
 urlpatterns = [
     path('', include('manager.urls')),
     path('admin/', admin.site.urls),
     path('users/', include('django.contrib.auth.urls')),
 ]
-#if settings.DEBUG:
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
+]
