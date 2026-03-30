ab72209bb3910fbe627b3bae4ef7f225c8d09fa3
jjk4 <shaun7090@gmail.com>
2026-03-30 10:12:04 +0200

    make debug default to false

--- opencent/settings.py
+++ opencent/settings.py
@@ -31,7 +31,7 @@ if os.path.isfile(dotenv_file):
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False').lower() in ('true', '1', 't')
 
 ALLOWED_HOSTS = ["*"]
 
