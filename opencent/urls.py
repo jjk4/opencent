9a68799a2285e344de6f7de9d003580f3ad8ca67
jjk4 <shaun7090@gmail.com>
2026-03-30 10:29:17 +0200

    fix for media files

--- opencent/urls.py
+++ opencent/urls.py
@@ -25,5 +25,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('django.contrib.auth.urls')),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#if settings.DEBUG:
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
